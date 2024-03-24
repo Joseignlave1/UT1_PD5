56aa821109a7c12e3499e9ad6e31ecf075fe1445
Jose Ignacio Lavecchia Suarez <127800259+Joseignlave1@users.noreply.github.com>
2024-03-24 16:19:45 -0300

    Ejercicio9

---
+++ src/Ejercicio9.java
@@ -0,0 +1,11 @@
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        String hannah = "Did Hannah see bees? Hannah did.";
+        //a) La expresión "hannah.length()" muestra un valor de 32
+        System.out.println(hannah.length());
+        //b) el valor retornado por la invocación del método "hannah.charAt(12)" es "e"
+        System.out.println(hannah.charAt(12));
+        //c)Escribe una expresión que referencie la letra “b” en la string referida por “hannah".
+        System.out.println(hannah.charAt(15));
+    }
+}
